4dc12de16147af4f65d06eb5756464789d0949d6
Clayton Yochum <claytonjy@gmail.com>
2016-04-19 17:07:27 -0400

    Refactored zelig_prediction_summary to break helpers into separate functions.
    Made other changes in response to more informal testing.

--- R/zelig_prediction_summary.R
+++ R/zelig_prediction_summary.R
@@ -2,6 +2,8 @@
 #'
 #' @param models
 #'
+#' @importFrom magrittr %>%
+#'
 #' @export
 zelig_prediction_summary <- function(model) {
 
@@ -10,34 +12,44 @@ zelig_prediction_summary <- function(model) {
     stop("model must be a Zelig model")
   }
 
-  # isolate simulations
-  sims <- model$sim.out
+  # isolate simulations and names
+  sims      <- model$sim.out
+  sim_names <- names(sims)
 
-  # per-simulation function
-  f <- function(sim) {
+  # get per-simulation tbl_df's & combine
+  Map(summarize_zelig_simulation, sims, sim_names) %>% dplyr::bind_rows()
+}
 
-    # isolate evs
-    evs <- sim$ev
 
-    # per-condition function
-    g <- function(ev) {
-      dplyr::data_frame(
-        lower = quantile(ev, 0.025),
-        mean  = mean(ev),
-        upper = quantile(ev, 0.975)
-      )
-    }
+#### helper functions ####
 
-    # get a tbl_df with columns (condition, lower, mean, upper) and as many rows as evs
-    evs %>%
-      lapply(g) %>%
-      bind_rows %>%
-      bind_cols(data_frame(condition = rownames(sim)), .)
-  }
+# usage: summarize_zelig_simulation(z$sim.out$x)
+summarize_zelig_simulation <- function(sim, name) {
 
-  # get per-simulation tbl_df's, combine, prepend simulation names
-  sims %>%
-    lapply(f) %>%
-    bind_rows %>%
-    bind_cols(data_frame(simulation = names(sims)), .)
+  # get names of different conditions and their expected values
+  evs        <- sim$ev[[1]]
+  conditions <- dimnames(evs)[2] %>% unlist
+  if (is.null(conditions)) conditions <- 1:(dim(evs)[2])  # default
+
+  # this function has only been tested where evs is a matrix (2d) or
+  # 3D array where the third dimension is 1; throw error in other cases
+  stopifnot(length(dim(evs)) < 3 | dim(evs)[3] == 1)
+
+  # apply the condition function to the second dimension
+  results <- evs %>% apply(2, summarize_zelig_condition)
+
+  # combine results and prepend condition names
+  results %>%
+    dplyr::bind_rows() %>%
+    dplyr::bind_cols(dplyr::data_frame(simulation = rep(name, length(results)), condition = conditions), .)
+}
+
+
+# usage: summarize_zelig_condition(z$sim.out$x$ev[[1]], rownames(z$sim.out$x)[1])
+summarize_zelig_condition <- function(x) {
+  dplyr::data_frame(
+    lower     = quantile(x, 0.025),
+    mean      = mean(x),
+    upper     = quantile(x, 0.975)
+  )
 }
